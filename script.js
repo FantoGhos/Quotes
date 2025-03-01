23a35ab1425f1d85023dc35a3f462e2d0d94b23c
FantoGhos <andrijmarcuk288@gmail.com>
2025-03-01 16:14:47 +0100

    global-update-3

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ fetch('./quote.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        initializeQuotes(data);
+        if (data && data.quotes) {
+            initializeQuotes(data);
+        } else {
+            console.error('Invalid JSON structure');
+        }
     })
     .catch(error => console.error('Error loading JSON:', error));
 
@@ -18,7 +22,36 @@ function initializeQuotes(data) {
             const selectedQuote = quotes[randomIndex];
             quoteDisplay.innerHTML = `<em>"${selectedQuote.quote}"</em>`;
             authorDisplay.innerHTML = `-${selectedQuote.author}`;
-            console.log(randomIndex);
+            clickOfButton();
         });
+    }
+}
+
+const numberOfClicks = document.getElementsByClassName('number-of-clicks')[0];
+let clickCount = 0;
+
+function clickOfButton() {
+    clickCount++;
+    if (numberOfClicks) {
+        numberOfClicks.innerHTML = `Кількість кліків: ${clickCount}`;
+    }
+    alertMessage();
+}
+
+function alertMessage() {
+    if(clickCount === 10) {
+        alert("Ви вже 10 разів натиснули на кнопку!");
+    }
+    if (clickCount === 50) {
+        alert("Фух, ви вже натиснули 50 разів на кнопку!");
+    }
+    if (clickCount === 100) {
+        alert("Ви шо тю тю? Ви вже 100 разів натиснули на кнопку...");
+    }
+    if (clickCount === 500) {
+        alert("Ви вже 500 разів натиснули на кнопку! Вам не набридло?");
+    }
+    if (clickCount === 1000) {
+        alert("Ти йобнувся? 1000 разів натиснути на кнопку? Це вже не нормально...");
     }
 }
