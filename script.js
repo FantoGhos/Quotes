c8b43b7135a2e3db588f0737656598d40b8adbfc
FantoGhos <andrijmarcuk288@gmail.com>
2025-03-01 14:17:03 +0100

    add favicon, update-style

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ fetch('./quote.json')
 
 function initializeQuotes(data) {
     const quotes = data.quotes;
-
     const quoteButton = document.getElementById('quoteButton');
     const quoteDisplay = document.getElementById('quoteDisplay');
+
     if (quoteButton && quoteDisplay) {
         quoteButton.addEventListener('click', () => {
             const randomIndex = Math.floor(Math.random() * quotes.length);
             const selectedQuote = quotes[randomIndex];
             quoteDisplay.innerHTML = `"${selectedQuote.quote}" - <em>${selectedQuote.author}</em>`;
+            console.log(randomIndex);
         });
     }
 }
